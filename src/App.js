99e16980d0336c86066712e0f3868885fe1e6abb
nikkhil31 <nikhilimbad@gmail.com>
2021-11-10 17:45:14 +0530

    front page done

--- src/App.js
+++ src/App.js
@@ -1,18 +1,253 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import { FaSearch, FaPlus, FaMinus, FaRedo } from 'react-icons/fa'
 
 function App() {
-  return (
-   <>
-    <header className="header container">
-      <h2>KAKA Sky</h2>
-    </header>
-    <main className='container'>
-      <div></div>
-      <div></div>
-    </main>
-   </>
-  );
+    return (
+        <>
+            <header className="header container">
+                <h2>KAKA Sky</h2>
+            </header>
+            <main className="container main">
+                <div className="main-list">
+                    <h3>Search Channel</h3>
+                    <div className="search-input">
+                        <input type="text" placeholder="Search" name="search" />
+                        <FaSearch size="30" className="search" />
+                    </div>
+                    <div className="channels-list">
+                        <div className="channel">
+                            <p>Start Plus - Rs 12</p>
+                            <div className="icon">
+                                <FaPlus className="plus-icon" />
+                            </div>
+                        </div>
+                        <div className="channel">
+                            <p>Start Plus - Rs 12</p>
+                            <div className="icon">
+                                <FaPlus className="plus-icon" />
+                            </div>
+                        </div>
+                        <div className="channel">
+                            <p>Start Plus - Rs 12</p>
+                            <div
+                                className="icon"
+                                style={{ border: '2px solid #fa6868' }}
+                            >
+                                <FaMinus className="minus-icon" />
+                            </div>
+                        </div>
+                        <div className="channel">
+                            <p>Start Plus - Rs 12</p>
+                            <div className="icon">
+                                <FaPlus className="plus-icon" />
+                            </div>
+                        </div>
+                        <div className="channel">
+                            <p>Start Plus - Rs 12</p>
+                            <div className="icon">
+                                <FaPlus className="plus-icon" />
+                            </div>
+                        </div>
+                        <div className="channel">
+                            <p>Start Plus - Rs 12</p>
+                            <div className="icon">
+                                <FaPlus className="plus-icon" />
+                            </div>
+                        </div>
+                        <div className="channel">
+                            <p>Start Plus - Rs 12</p>
+                            <div className="icon">
+                                <FaPlus className="plus-icon" />
+                            </div>
+                        </div>
+                        <div className="channel">
+                            <p>Start Plus - Rs 12</p>
+                            <div className="icon">
+                                <FaPlus className="plus-icon" />
+                            </div>
+                        </div>
+                        <div className="channel">
+                            <p>Start Plus - Rs 12</p>
+                            <div className="icon">
+                                <FaPlus className="plus-icon" />
+                            </div>
+                        </div>
+                        <div className="channel">
+                            <p>Start Plus - Rs 12</p>
+                            <div className="icon">
+                                <FaPlus className="plus-icon" />
+                            </div>
+                        </div>
+                        <div className="channel">
+                            <p>Start Plus - Rs 12</p>
+                            <div className="icon">
+                                <FaPlus className="plus-icon" />
+                            </div>
+                        </div>
+                        <div className="channel">
+                            <p>Start Plus - Rs 12</p>
+                            <div className="icon">
+                                <FaPlus className="plus-icon" />
+                            </div>
+                        </div>
+                        <div className="channel">
+                            <p>Start Plus - Rs 12</p>
+                            <div className="icon">
+                                <FaPlus className="plus-icon" />
+                            </div>
+                        </div>
+                        <div className="channel">
+                            <p>Start Plus - Rs 12</p>
+                            <div className="icon">
+                                <FaPlus className="plus-icon" />
+                            </div>
+                        </div>
+                        <div className="channel">
+                            <p>Start Plus - Rs 12</p>
+                            <div className="icon">
+                                <FaPlus className="plus-icon" />
+                            </div>
+                        </div>
+                        <div className="channel">
+                            <p>Start Plus - Rs 12</p>
+                            <div className="icon">
+                                <FaPlus className="plus-icon" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="main-list mainitem2">
+                    <h3>Selected Channels</h3>
+                    <div className="channels-list">
+                        <div className="channel">
+                            <p>Start Plus - Rs 12</p>
+                            <div
+                                className="icon"
+                                style={{ border: '2px solid #fa6868' }}
+                            >
+                                <FaMinus className="minus-icon" />
+                            </div>
+                        </div>
+                        <div className="channel">
+                            <p>Start Plus - Rs 12</p>
+                            <div
+                                className="icon"
+                                style={{ border: '2px solid #fa6868' }}
+                            >
+                                <FaMinus className="minus-icon" />
+                            </div>
+                        </div>
+                        <div className="channel">
+                            <p>Start Plus - Rs 12</p>
+                            <div
+                                className="icon"
+                                style={{ border: '2px solid #fa6868' }}
+                            >
+                                <FaMinus className="minus-icon" />
+                            </div>
+                        </div>
+                        <div className="channel">
+                            <p>Start Plus - Rs 12</p>
+                            <div
+                                className="icon"
+                                style={{ border: '2px solid #fa6868' }}
+                            >
+                                <FaMinus className="minus-icon" />
+                            </div>
+                        </div>
+                        <div className="channel">
+                            <p>Start Plus - Rs 12</p>
+                            <div
+                                className="icon"
+                                style={{ border: '2px solid #fa6868' }}
+                            >
+                                <FaMinus className="minus-icon" />
+                            </div>
+                        </div>
+                        <div className="channel">
+                            <p>Start Plus - Rs 12</p>
+                            <div
+                                className="icon"
+                                style={{ border: '2px solid #fa6868' }}
+                            >
+                                <FaMinus className="minus-icon" />
+                            </div>
+                        </div>
+                        <div className="channel">
+                            <p>Start Plus - Rs 12</p>
+                            <div
+                                className="icon"
+                                style={{ border: '2px solid #fa6868' }}
+                            >
+                                <FaMinus className="minus-icon" />
+                            </div>
+                        </div>
+                        <div className="channel">
+                            <p>Start Plus - Rs 12</p>
+                            <div
+                                className="icon"
+                                style={{ border: '2px solid #fa6868' }}
+                            >
+                                <FaMinus className="minus-icon" />
+                            </div>
+                        </div>
+                        <div className="channel">
+                            <p>Start Plus - Rs 12</p>
+                            <div
+                                className="icon"
+                                style={{ border: '2px solid #fa6868' }}
+                            >
+                                <FaMinus className="minus-icon" />
+                            </div>
+                        </div>
+                        <div className="channel">
+                            <p>Start Plus - Rs 12</p>
+                            <div
+                                className="icon"
+                                style={{ border: '2px solid #fa6868' }}
+                            >
+                                <FaMinus className="minus-icon" />
+                            </div>
+                        </div>
+                        <div className="channel">
+                            <p>Start Plus - Rs 12</p>
+                            <div
+                                className="icon"
+                                style={{ border: '2px solid #fa6868' }}
+                            >
+                                <FaMinus className="minus-icon" />
+                            </div>
+                        </div>
+                        <div className="channel">
+                            <p>Start Plus - Rs 12</p>
+                            <div
+                                className="icon"
+                                style={{ border: '2px solid #fa6868' }}
+                            >
+                                <FaMinus className="minus-icon" />
+                            </div>
+                        </div>
+                        <div className="channel">
+                            <p>Start Plus - Rs 12</p>
+                            <div
+                                className="icon"
+                                style={{ border: '2px solid #fa6868' }}
+                            >
+                                <FaMinus className="minus-icon" />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="footer">
+                        <div className="restore">
+                            <FaRedo />
+                        </div>
+                        <div className="submit">SUBMIT</div>
+                    </div>
+                </div>
+            </main>
+        </>
+    )
 }
 
-export default App;
+export default App
